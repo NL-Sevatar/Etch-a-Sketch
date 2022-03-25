564986a01557ed764c649eb104b92fad3307a545
Oden <Nada>
2022-03-25 11:10:18 -0700

    Added a create and clear grid button with working functions

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const grid = document.querySelector('#container');
 const base = document.createElement('div');
+const elem = document.querySelectorAll('.blank')
    
-function sGrid(num) {
+function createGrid() {
+    let num = prompt('',16)
     for (let i = 0; i < num; i++){
         base.classList.add('blank');
         grid.appendChild(base.cloneNode(true));
     }
 }
+
+function clearGrid() {
+    document.querySelectorAll('.blank').forEach(e => e.remove())
+    }
